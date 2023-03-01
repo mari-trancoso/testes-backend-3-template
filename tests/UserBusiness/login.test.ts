a219aced3f1fa5b32ed3fe208efbb5d1c6f09b93
mari-trancoso <mnegraotrancoso@gmail.com>
2023-03-01 11:11:20 -0300

    Implementado os testes para os erros.

--- tests/UserBusiness/login.test.ts
+++ tests/UserBusiness/login.test.ts
@@ -1,5 +1,7 @@
 import { UserBusiness } from "../../src/business/UserBusiness"
 import { LoginInputDTO } from "../../src/dtos/userDTO"
+import { BadRequestError } from "../../src/errors/BadRequestError"
+import { NotFoundError } from "../../src/errors/NotFoundError"
 import { HashManagerMock } from "../mocks/HashManagerMock"
 import { IdGeneratorMock } from "../mocks/IdGeneratorMock"
 import { TokenManagerMock } from "../mocks/TokenManagerMock"
@@ -31,5 +33,56 @@ describe("login", () => {
 
         const response = await userBusiness.login(input)
         expect(response.token).toBe("token-mock-admin")
+    })
+
+    test("deve dar erro se email não for string", async () => {
+        expect.assertions(1)
+        try{
+            const input: LoginInputDTO = {
+                email: null,
+                password: "bananinha"
+            }
+
+            await userBusiness.login(input)
+
+        }catch(error){
+            if(error instanceof BadRequestError){
+                expect(error.message).toBe("'email' deve ser string")
+            }
+        }
+    })
+
+    test("deve disparar erro caso o email não seja encontrado", async () => {
+        expect.assertions(1)
+        try{
+            const input: LoginInputDTO = {
+                email: "null@email.com",
+                password: "bananinha"
+            }
+
+            await userBusiness.login(input)
+
+        }catch(error){
+            if(error instanceof NotFoundError){
+                expect(error.message).toBe("'email' não cadastrado")
+            }
+        }
+    })
+
+    test("deve disparar erro caso a password esteja errada", async () => {
+        expect.assertions(1)
+        try{
+            const input: LoginInputDTO = {
+                email: "normal@email.com",
+                password: "bananinhaerrado"
+            }
+
+            await userBusiness.login(input)
+
+        }catch(error){
+            if(error instanceof BadRequestError){
+                expect(error.message).toBe("'password' incorreto")
+            }
+        }
     })
 })
